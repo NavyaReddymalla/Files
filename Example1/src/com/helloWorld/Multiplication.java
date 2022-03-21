4404aeddb3e617d1c96def1059ea3acfc2d41319
reddy <reddy@DESKTOP-08J6GDJ>
2022-03-21 15:59:22 +0530

    push multiplication program

---
+++ Example1/src/com/helloWorld/Multiplication.java
@@ -0,0 +1,30 @@
+package com.helloWorld;
+
+public class Multiplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=5,b=5,c;
+		c=a*b;
+		System.out.println("product of a and b is: "+c);
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+
+	}
+
+}
