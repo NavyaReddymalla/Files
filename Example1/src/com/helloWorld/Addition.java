6f774ed4d43a502cd9273fb043951d327330226e
reddy <reddy@DESKTOP-08J6GDJ>
2022-03-21 15:46:43 +0530

    Save java program

---
+++ Example1/src/com/helloWorld/Addition.java
@@ -0,0 +1,13 @@
+package com.helloWorld;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10,b=20,c;
+		c=a+b;
+		System.out.println("sum of a and b: "+c);
+
+	}
+
+}
